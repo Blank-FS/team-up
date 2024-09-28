4b0d8a401ad256b8d6693ed53c8e8aede4a3f13d
Blank-FS <137102716+Blank-FS@users.noreply.github.com>
2024-09-28 17:55:58 -0400

    wip createProfile function

--- lib/types.ts
+++ lib/types.ts
@@ -1,25 +1,23 @@
-
-type User = {
-   id: number;
-   user_name: string;
-   password: string;
-   first_name: string;
-   last_name: string;
-   email: string;
-   school: string;
-   teams: Team[];
-   skills: Skill[];
+export type User = {
+  id?: string;
+  user_name: string;
+  password: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  school: string;
+  teams: Team[];
+  skills: Skill[];
 };
 
-type Team = {
-   id: number;
-   team_name: string;
-   users: User[];
-   skills: Skill[];
+export type Team = {
+  id: number;
+  team_name: string;
+  users: User[];
+  skills: Skill[];
 };
 
-type Skill = {
-   id: number;
-   skill_name: string;
-   category: String;
-}
+export type Skill = {
+  id: number;
+  skill_name: string;
+};
--- lib/utils/users.ts
+++ lib/utils/users.ts
@@ -1,15 +1,31 @@
 import { getSession } from "@auth0/nextjs-auth0";
 import prisma from "../../prisma/db";
+import { User, Skill, Team } from "../types";
 
 // Check if current user in session has profile, return true if user has profile
 export async function hasProfile() {
   const session = await getSession();
   const user = session?.user;
   const email = user?.email;
-  const profile = await prisma.users.findUnique({
+  const profile = await prisma.user.findUnique({
     where: {
       email: email,
     },
   });
   return profile ? true : false;
+}
+
+export async function createProfile(formData: User) {
+  const session = await getSession();
+  const user = session?.user;
+
+  const profile = await prisma.user.create({
+    data: {
+      email: formData.email,
+      first_name: formData.first_name,
+      last_name: formData.last_name,
+      user_name: formData.user_name,
+      school: formData.school,
+    },
+  });
 }
