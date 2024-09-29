e130913a576ddee11bc81be626125955790c89ed
Kenny Li <kennyli306@gmail.com>
2024-09-29 05:22:47 -0400

    fix dashboard

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,33 @@
-import { getUserByEmail, getAllUsers, getInfo } from "@/lib/utils/users";
+import { getUserByEmail, getAllUsers, getInfo, createProfile } from "@/lib/utils/users";
 import { getAllTeams } from "@/lib/utils/teams";
 import { UserExtra } from "@/lib/types";
 import { User } from "@prisma/client";
 import Tab from "@/components/dashboard/Tab";
+import { UserForm } from "@/lib/forms";
+import { UserProfile } from "@auth0/nextjs-auth0/client";
 
 export default async function Dashboard() {
-  const currSessionUser = await getInfo();
-  const currentUser: UserExtra | null = await getUserByEmail(currSessionUser.email);
+  const sessUser = await getInfo() as UserProfile;
+  const currentUser: UserExtra | null = await getUserByEmail(sessUser.email as string);
   const availableUsers : User[] | null = await getAllUsers();
   const teams = await getAllTeams();
+
+  if (currentUser == null) {
+    let userForm : UserForm = {
+      user_id: "",
+      user_name: sessUser.nickname as string,
+      first_name: "",
+      last_name: "",
+      avatar: sessUser.picture as string,
+      role: "",
+      bio: "",
+      email: sessUser.email as string,
+      school: "",
+      skills: []
+    }
+
+    await createProfile(userForm);
+  }
 
   return (
     <Tab
